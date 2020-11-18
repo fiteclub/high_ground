d7c45171503197591c8c3be675efb4f5af004791
fiteclub <mattf255@yahoo.com>
2020-11-18 00:20:41 -0500

    Add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,19 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)jjj
+data = YAML.safe_load(File.read('/Users/matt/repos/HighGround/db/celebrities.yml')).deep_symbolize_keys
+data[:celebrities].each do |celebrity|
+  location = Location.create(
+    address_1: celebrity[:address_1],
+    address_2: celebrity[:address_2],
+    city: celebrity[:city],
+    state: celebrity[:state],
+    zip: celebrity[:zip]
+  )
+  Combatant.create(
+    first_name: celebrity[:first_name],
+    last_name: celebrity[:last_name],
+    location: location
+  )
+end
