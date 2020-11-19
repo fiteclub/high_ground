d0c611db3f6238877558913bf648eeb1bcc107ab
fiteclub <mattf255@yahoo.com>
2020-11-19 05:07:23 -0500

    Add elevation lookup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Warrior.create(
+  title: "George Lucas at Skywalker Ranch",
+  address: "5858 Lucas Valley Rd, Nicasio, CA 94946",
+  latitude: 38.063269,
+  longitude: -122.652595
+)
+
+Warrior.create(
+  title: "Chris Hadfield aboard the ISS",
+  address: "190 Oak Manor Drive, Cape Canaveral, Florida 32920, United States",
+  latitude: 28.396837,
+  longitude: -80.605659,
+  elevation: 400000
+)
+
+Warrior.create(
+  title: "Victor Vescovo aboard a Submarine",
+  address: "Challenger Deep, Mariana Trench, North Pacific Ocean",
+  latitude: 11.373333,
+  longitude: 142.591667,
+  elevation: -10927
+)
