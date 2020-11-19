70484f15faf6ddd0830fcf7e6b5e5294d21a1c50
fiteclub <mattf255@yahoo.com>
2020-11-18 19:33:10 -0500

    Initialize gecoder

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ApplicationRecord
   has_many :combatants
+  geocoded_by :full_address, latitude: :latitude, longitude: :longitude
+  after_validation :geocode
+
+  def full_address
+    [address_1, address_2, city, state, zip].compact.join(', ')
+  end
 end
