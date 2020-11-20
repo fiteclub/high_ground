14ff4cec637ed838e4e2fae1d0c20dbfe0634d3a
fiteclub <mattf255@yahoo.com>
2020-11-19 22:33:33 -0500

    Refactor warrior model

--- app/models/warrior.rb
+++ app/models/warrior.rb
@@ -1,15 +1,31 @@
 class Warrior < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
-  after_validation :reverse_geocode, unless: ->(obj) { obj.address.present? },
-                    if: ->(obj){ obj.latitude.present? and obj.latitude_changed? and obj.longitude.present? and obj.longitude_changed? }
-  after_validation :get_elevation, unless: ->(obj) { obj.elevation.present? },
-                    if: ->(obj){ obj.latitude.present? and obj.latitude_changed? and obj.longitude.present? and obj.longitude_changed? }
+  after_validation :fetch_geo_data
+
+  def fetch_geo_data
+    geocode if updated_address?
+    reverse_geocode if updated_coordinates? && address.blank?
+    get_elevation if updated_coordinates?
+  end
 
   def get_elevation
     query="https://api.elevationapi.com/api/Elevation?lat=#{latitude}&lon=#{longitude}&dataSet=SRTM_GL3"
     json = Net::HTTP.get_response(URI(query)).body
     self.elevation = JSON.parse(json)["geoPoints"][0]["elevation"]
+  end
+
+  private
+
+  def updated_coordinates?
+    latitude.present? && 
+    latitude_changed? && 
+    longitude.present? && 
+    longitude_changed?
+  end
+
+  def updated_address?
+    address.present? &&
+    address_changed?
   end
   
 end
