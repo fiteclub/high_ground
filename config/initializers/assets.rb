fa2fb30b691270c614b304ecd0ce57a07b462487
fiteclub <mattf255@yahoo.com>
2020-11-24 20:35:50 -0500

    Refactor asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,10 +7,9 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'videos')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( flame_effect.mp4 )
