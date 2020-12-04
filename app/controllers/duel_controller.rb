5c57b0bbddd20778b4dd2532b95ee77bdfbe245c
fiteclub <mattf255@yahoo.com>
2020-12-04 00:37:14 -0500

    Add form, routes, controller

---
+++ app/controllers/duel_controller.rb
@@ -0,0 +1,10 @@
+class DuelController < ApplicationController
+  
+  def fart
+    binding.pry
+    # @player_one = Warrior.new(title: "Player One", address: form_input)
+    # @player_one = Warrior.new(title: "Player One", address: form_input)
+    # @results = Warrior.duel(@player_one, @player_two)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :warriors do
     get 'pvp', on: :collection
   end
+  post 'fart', to: 'duel#fart', as: 'fart'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
