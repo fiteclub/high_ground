5c9a96e5536ea56df5d9becbd11f11f3e992890d
fiteclub <mattf255@yahoo.com>
2020-12-30 19:48:00 -0500

    Fix Heroku conflicts

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
-environment.loaders.prepend('erb', erb)
+environment.loaders.prepend('erb', {
+  test: /\.erb$/,
+  enforce: 'pre',
+  use: [{
+    loader: 'rails-erb-loader',
+  }]
+})
 module.exports = environment
